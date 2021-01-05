7f5f8e9d0b22e961018eb2db70bbf5fc81430ee7
szlatyka <kimmelgabor@gmail.com>
2021-01-05 14:46:34 +0100

    Added db connection

---
+++ src/DbConnection.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, VARCHAR, BLOB, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker
+Base = declarative_base()
+
+engine = create_engine('sqlite:///sqlapi.db', echo=True)
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+#user = User()
+#user.id = 0
+#user.name = "Bob"
+#session.add(user)
+#session.commit()
+#session.close()
