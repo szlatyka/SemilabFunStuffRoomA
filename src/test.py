546a65ed31b63b2b320249b03096e8f3dcc48244
Szlatyka <kimmelgabor@gmail.com>
2021-01-05 17:00:57 +0100

    Added little test

--- src/test.py
+++ src/test.py
@@ -1,1 +1,15 @@
-kkk
+import Api
+
+#Api.User.Create("gábor", "absc")
+usr = Api.User.ByName("gábor")
+isr2 = Api.User(1);
+
+print("#ID: " + str(isr2.name()))
+
+#Api.Group.Create("Test csopi")
+grp = Api.Group(1);
+
+grp.assign(usr)
+print();
+grp.remove(usr)
+print("#ID: " + str(usr.group()));
