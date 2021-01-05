e5c3643fb6a19368593918dc70db8d1fcacd8968
Áron Horváth <aron.horvath@semilab.hu>
2021-01-05 17:05:53 +0100

    test

--- src/test.py
+++ src/test.py
@@ -1,10 +1,10 @@
 import Api
 
 print(Api.Group.All())
-#
-# Api.Group.Create('Sorter')
-# print(Api.Group.All())
-#
-# Api.Group.Create('Analysis')
-# print(Api.Group.All())
+
+Api.Group.Create('Sorter')
+print(Api.Group.All())
+
+Api.Group.Create('Analysis')
+print(Api.Group.All())
 
